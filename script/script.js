baba3737bd0e20770f1c06c276d2332ba6366085
David4bay <david4bay@gmail.com>
2023-01-22 22:16:12 +0100

    added JavaScript DOM commands and further scss styling

--- script/script.js
+++ script/script.js
@@ -3,3 +3,32 @@
 Add functionality in pure JavaScript DOM and leave comments on jQuery implementation.
 
 */
+
+
+const btn = document.querySelector('.numbers');
+
+const btnValue = btn.innerHTML;
+
+const result = document.querySelector('.result');
+
+const resultValue = result.innerHTML;
+
+const equals = document.querySelector('.equals');
+
+const clear = document.querySelector('.clear');
+
+const empty = '';
+
+const clearValue = clear.innerHTML;
+
+btn.addEventListener('click', (e) => {
+    resultValue = btnValue;
+});
+
+equals.addEventListener('click', (e) => {
+    +resultValue;
+})
+
+clear.addEventListener('click', (e) => {
+    resultValue = empty;
+})
