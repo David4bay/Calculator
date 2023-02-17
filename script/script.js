66960f203a6a3f5b9daf2eed59ee8c4f65ef8b79
David4bay <david4bay@gmail.com>
2023-02-18 00:37:36 +0100

    changed eval function

--- script/script.js
+++ script/script.js
@@ -32,7 +32,7 @@ division.addEventListener("click", (e) => {
 const equals = document.getElementById("equals");
 
 equals.addEventListener("click", (e) => {
-    document.getElementById("result").innerText =  eval(document.getElementById("result").innerText.replace(/[^**-+/*\d]/g, ''));                                      
+    document.getElementById("result").innerText =  eval(document.getElementById("result").innerText);                                      
 });
 
 const clear = document.getElementById("clear");
